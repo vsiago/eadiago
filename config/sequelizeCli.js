cb14e71ce14e1e92cf21d8e94ad3aa02e634fd6d
Iago Souza Vieira <iago.vieira@itg.rio>
2023-11-07 16:20:37 -0300

    config inicial

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: 'localholst',
+        port: '5432',
+        database: 'eadiago_development',
+        username: 'eadiago',
+        password: 'eadiago'
+    }
+}
